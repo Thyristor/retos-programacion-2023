4ba51cdaee55294108582fe58f582ad519f54aea
Javier Jimenez Perez <jjp.javierjimenezperez@gmail.com>
2023-12-27 15:22:15 +0100

    Reto #1: EL "LENGUAJE HACKER"

--- Retos/Reto #1 - EL LENGUAJE HACKER [Fácil]/python/thyristor_#002.py
+++ Retos/Reto #1 - EL LENGUAJE HACKER [Fácil]/python/thyristor_#002.py
@@ -1,26 +1,42 @@
 #/*
-# * Escribe una función que reciba dos palabras (String) y retorne
-# * verdadero o falso (Bool) según sean o no anagramas.
-# * - Un Anagrama consiste en formar una palabra reordenando TODAS
-# *   las letras de otra palabra inicial.
-# * - NO hace falta comprobar que ambas palabras existan.
-# * - Dos palabras exactamente iguales no son anagrama.
+# * Escribe un programa que reciba un texto y transforme lenguaje natural a
+# * "lenguaje hacker" (conocido realmente como "leet" o "1337"). Este lenguaje
+# *  se caracteriza por sustituir caracteres alfanuméricos.
+# * - Utiliza esta tabla (https://www.gamehouse.com/blog/leet-speak-cheat-sheet/) 
+# *   con el alfabeto y los números en "leet".
+# *   (Usa la primera opción de cada transformación. Por ejemplo "4" para la "a")
 # */
 
-def funcion(string_1: str, string_2: str):
-    if string_1 == string_2:
-        print("Las dos palbras son iguales")
-        return False
-    elif string_1[::-1] == string_2:
-        print("Es un anagrama")
-        return True
-    else:
-        print("No es un anagrama")
-        return False
-
 if __name__ == "__main__":
-    string_1 = input("Introduce el primer string: ")
-    string_2 = input("Introduce el segundo string: ")
+    texto = input("Introduce un texto: ")
+    dictionary = {
+        "A": "4",
+        "B": "I3",
+        "C": "[",
+        "D": ")",
+        "E": "3",
+        "F": "|=",
+        "G": "&",
+        "H": "#",
+        "I": "1",
+        "J": ",_|",
+        "K": ">|",
+        "L": "1",
+        "M": "/\\/\\",
+        "N": "^/",
+        "O": "0",
+        "P": "|*",
+        "Q": "(_,)",
+        "R": "I2",
+        "S": "5",
+        "T": "7",
+        "U": "(_)",
+        "V": "\/",
+        "W": "\/\/",
+        "X": "><"
+    }
 
-    booleano = funcion(string_1, string_2)
-    print(f"Booleano: {booleano}")
+    print(texto)
+    for key, val in dictionary.items():
+        texto = texto.upper().replace(key, val)
+    print(texto)
